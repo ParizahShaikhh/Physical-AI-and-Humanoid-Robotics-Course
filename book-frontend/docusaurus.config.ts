dc06656825aa826ce9d3279308f129902479edbc
ParizahShaikhh <137597244+ParizahShaikhh@users.noreply.github.com>
2025-12-24 17:49:25 +0500

    .

--- book-frontend/docusaurus.config.ts
+++ book-frontend/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://physical-ai-and-humanoid-robotics-c-flame.vercel.app/',
+  url: 'https://vercel.com/parizah-shaikhs-projects/physical-ai-and-humanoid-robotics-course-book1', // Your website URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub Pages deployment, use the repository name
   baseUrl: '/',
